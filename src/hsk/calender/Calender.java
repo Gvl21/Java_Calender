38adf195c830bf940e6e0e50667bd9a43cda07d7
Gvl21 <thornyk1@gmail.com>
2023-08-04 17:11:12 +0900

    add simple i/o

--- src/hsk/calender/Calender.java
+++ src/hsk/calender/Calender.java
@@ -1,7 +1,23 @@
 package hsk.calender;
 
+import java.util.Scanner;
+
 public class Calender {
     public static void main(String[] args) {
-        System.out.println("hello calender");
+        System.out.println(" 일 월 화 수 목 금 토 ");
+        System.out.println("--------------------");
+        System.out.println(" 1  2  3  4  5  6  7 ");
+        System.out.println(" 8  9 10 11 12 13 14 ");
+        System.out.println("15 16 17 18 19 20 21 ");
+        System.out.println("22 23 24 25 26 27 28 ");
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("월을 입력하세요>");
+        int month = scn.nextInt();
+
+        int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.printf("%d월의 마지막 날은 %d입니다.", month, maxDays[month-1]);
+
+
     }
 }
