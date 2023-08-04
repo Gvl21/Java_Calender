bd7ac250e7c63e7dedfc87923c2dacb958f8c810
Gvl21 <thornyk1@gmail.com>
2023-08-04 17:23:23 +0900

    add simple methods

--- src/hsk/calender/Calender.java
+++ src/hsk/calender/Calender.java
@@ -3,20 +3,29 @@ package hsk.calender;
 import java.util.Scanner;
 
 public class Calender {
-    public static void main(String[] args) {
+    private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public void maxDaysOfMonth(int month) {
+        System.out.printf("%d월의 마지막 날은 %d입니다.", month, MAX_DAYS[month - 1]);
+    }
+
+    public void sampleCal() {
         System.out.println(" 일 월 화 수 목 금 토 ");
         System.out.println("--------------------");
         System.out.println(" 1  2  3  4  5  6  7 ");
         System.out.println(" 8  9 10 11 12 13 14 ");
         System.out.println("15 16 17 18 19 20 21 ");
         System.out.println("22 23 24 25 26 27 28 ");
+    }
 
+    public static void main(String[] args) {
+        Calender cal = new Calender();
         Scanner scn = new Scanner(System.in);
-        System.out.println("월을 입력하세요>");
-        int month = scn.nextInt();
+        cal.sampleCal();
+        System.out.print("월을 입력하세요>");
 
-        int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        System.out.printf("%d월의 마지막 날은 %d입니다.", month, maxDays[month-1]);
+        int month = scn.nextInt();
+        cal.maxDaysOfMonth(month);
 
 
     }
